9cf4fdaec83eb4ed90d555ab1b65a47f5826caf7
Richard Zhang <zhanr0572@wrdsb.ca>
2025-01-12 12:21:10 -0500

    scroll frame with mousewheel

--- scrollFrame.py
+++ scrollFrame.py
@@ -4,22 +4,25 @@ from tkinter import ttk
 paragraph_elements = []
 class ScrollableFrame():
     def __init__(self, container, height, width, *args, **kwargs):
-        canvas = tk.Canvas(container, highlightbackground="white")
-        scrollbar = tk.Scrollbar(container, orient="vertical", command=canvas.yview)
-        self.scrollable_frame = ttk.Frame(canvas)
+        self.canvas = tk.Canvas(container, highlightbackground="white")
+        scrollbar = tk.Scrollbar(container, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = ttk.Frame(self.canvas)
 
         self.scrollable_frame.bind(
             "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
             )
         )
         paragraph_elements.append(self.scrollable_frame)
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
 
-        canvas.configure(yscrollcommand=scrollbar.set)
-
-        canvas.pack(side="left", fill="both", expand=True)
-        paragraph_elements.append(canvas)
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+        self.canvas.bind_all("<MouseWheel>", self.onMousewheel)
+        self.canvas.pack(side="left", fill="both", expand=True)
+        paragraph_elements.append(self.canvas)
         scrollbar.pack(side="right", fill="y")
         paragraph_elements.append(scrollbar)
+
+    def onMousewheel(self, event):
+        self.canvas.yview_scroll(int(-1 * (event.delta)), "units")
